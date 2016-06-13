14d653820d3f8e5ec6d8bd49e6ae07b80e6aa86d
vanJargon <vanessa_yun@mymail.sutd.edu.sg>
2016-06-14 01:15:24 +0800

    Minor Edits.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
-var side = prompt("Thanks for using Etch-A-Sketch! First off, how many rows do you want your sketch pad to span?", "16");
-
-function trace(){
-	$(this).closest('.pixels').css("background-color","#53585F");
-}
+var side = prompt("Hello! Thank you for using Etch-A-Sketch. First off, how many rows do you want your sketch pad to span?", "16");
 
 $(document).ready(function(){
 	for (var i=0;i<side;i++){
 		$('table').append($('<tr></tr>'));
 	}
 	for (var i=0;i<(7/4)*side;i++){
-		$('tr').append($('<td><div class="pixels"></div></td>'));
+		$('tr').append($('<td><div class="pixels '+i+'"></div></td>'));
 	}
 	$(".pixels").height($(".pixels").width());
 
-	$(".grid").on("mousedown", function(){
-		$(".pixels").on("mouseenter", trace);
+	$(".pixels").mousemove(function(e){
+		if (e.which==1)
+		{
+			$(this).css("background-color","#53585F");
+			$(this).css("opacity","1.0");
+			e.preventDefault(); //for Chrome
+		}
 	});
 });
